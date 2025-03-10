40de066ebb49472a73fb20417eda610f61a78382
Patrik Cesnek <pcesnek290@gmail.com>
2025-03-10 16:16:42 +0100

    Display TodoListView, call fetchTodos

---
+++ iosApp/iosApp/View/TodoListView.swift
@@ -0,0 +1,35 @@
+//
+//  TodoListView.swift
+//  iosApp
+//
+//  Created by Patrik Cesnek on 10/03/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct TodoListView: View {
+    @StateObject private var viewModel = TodoListViewModel()
+
+    var body: some View {
+        NavigationView {
+            List(viewModel.todos, id: \.id) { todo in
+                HStack {
+                    Button(action: {
+                        viewModel.toggleTodoCompletion(todo)
+                    }) {
+                        Image(systemName: todo.isCompleted ? "checkmark.square.fill" : "square")
+                            .foregroundColor(todo.isCompleted ? .green : .gray)
+                    }
+                    .buttonStyle(BorderlessButtonStyle())
+
+                    Text(todo.title)
+                        .strikethrough(todo.isCompleted, color: .gray)
+                        .foregroundColor(todo.isCompleted ? .gray : .primary)
+                }
+            }
+            .navigationTitle("To-Do List")
+        }
+    }
+}
